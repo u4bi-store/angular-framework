f9b74a794ae5ed893999abf6576f7528626a1611
u4bi <rootcode10@gmail.com>
2016-11-01 01:03:31 +0900

    angular 2.0 server init

---
+++ app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: `
+    <div>
+      <h1>angular2.0_workspace</h1>
+      <h2>angular2.0 test u4bi</h2>
+      https://angular.io/docs/ts/latest/quickstart.html
+    </div>
+    `
+})
+export class AppComponent {}
