eac6a24a13d20430b81be7eae55344395c870f80
u4bi <rootcode10@gmail.com>
2016-11-01 01:15:43 +0900

    the hero editor

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
 
+export class Hero {
+    id: number;
+    name: string;
+}
+
 @Component({
-  selector: 'my-app',
-  template: `
-    <div>
-      <h1>angular2.0_workspace</h1>
-      <h2>angular2.0 test u4bi</h2>
-      https://angular.io/docs/ts/latest/quickstart.html
-    </div>
-    `
+    selector: 'my-app',
+    template:`
+      <h1>{{title}}</h1>
+      <h2>{{hero.name}} details!</h2>
+      <div>
+        <label>id: </label>{{hero.id}}
+      </div>
+      <div>
+        <label>name: </label>
+        <input [(ngModel)]="hero.name" placeholder="name">
+      </div>
+      `
 })
-export class AppComponent {}
+export class AppComponent {
+    title = 'Tour of Heroes';
+    hero: Hero = {
+        id: 1,
+        name: 'Windstorm'
+    };
+
+}
