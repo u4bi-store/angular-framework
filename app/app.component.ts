f16dae92bb4ceb3da3c4d517352cf2257ebe50e7
u4bi <rootcode10@gmail.com>
2016-11-02 13:31:41 +0900

    angular2 init comment

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,13 +1,33 @@
 import { Component } from '@angular/core';
+/**
+ * @angular/core에 있는 Component 모듈을 import 한다.
+ * Component는 Angular에서 제공하는 모듈로
+ * 앞으로 가장 많이 사용하게 됨.
+ */
 
 @Component({
-  selector: 'my-app',
-  template: `
-    <div>
-      <h1>angular2.0_workspace</h1>
-      <h2>angular2.0 test u4bi</h2>
-      https://angular.io/docs/ts/latest/quickstart.html
-    </div>
-    `
+    /**
+     * selector는 HTML에서 해당 component가 들어갈
+     * tag 이름으로 사용됨. <my-app></my-app>
+     * 
+     * template은 화면을 그리는데 사용됨.
+     */
+    selector: 'my-app',
+    template: `
+        <div>
+        <h1>angular2.0_workspace dev2</h1>
+        <h2>angular2.0 test u4bi</h2>
+        <a href="https://github.com/u4bi/Angular2.0_workspace">깃허브드 앵귤러2 워크스페이스 브런치 dev2</a>
+        </div>
+        `
 })
-export class AppComponent {}
+
+export class AppComponent {
+
+}
+/**
+ * export class AppComponent{}는
+ * 
+ * AppComponent 클래스를 선언하고.
+ * export 해서 다른 곳에서 또 사용할 수 있다는걸 의미함.
+ */
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,6 +1,9 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }  from './app.component';
+/**
+ * app.component내의 AppComponent를 임포트함.
+*/
 @NgModule({
   imports: [
     BrowserModule
@@ -12,4 +15,17 @@ import { AppComponent }  from './app.component';
     AppComponent
   ]
 })
+/**
+ * ngModule 내에서의 Dependency는 공유된다
+ * 
+ * ngModule에서 dependency를 관리하고 실제 사용하는 dependency를 한 눈에 다 볼 수 있음.
+ * 뿐만 아니라, 코드도 더 간결해짐.
+ * 
+ * ngModule이 JavaScript의 module과 같이 module 단위로 scope를 갖음.
+ * 
+ * ngModule에 명시된 Component들은 ngModule에 명시된 dependencie 객체들
+ * 을 자유롭게 사용할 수 있다.
+ * 
+ * 부트스트래핑 AppComponent
+ */
 export class AppModule { }
