7c3f176bc9eb66f8336e2cd799cd22ea6733124f
u4bi <u4bi@u4bi.com>
2017-01-09 19:38:18 +0900

    rxjs extensions init search render

--- basic-web/src/app/hero-search.service.ts
+++ basic-web/src/app/hero-search.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs';
 
-import { Hero } from '.i/hero';
+import { Hero } from './hero';
 
 @Injectable()
 export class HeroSearchService {
