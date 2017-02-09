e9ceec68bf7a13bc39c52e0e0c2167793e85b20e
u4bi <u4bi@u4bi.com>
2017-02-09 17:33:55 +0900

    env dir firebase config ts init

---
+++ angular2fb2/src/environments/firebase.config.ts
@@ -0,0 +1,6 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCh1yJniqLoQ3D9A8BU8sfA4fwsS4ebDAc",
+    authDomain: "angular2test-bb3f2.firebaseapp.com",
+    databaseURL: "https://angular2test-bb3f2.firebaseio.com",
+    storageBucket: "angular2test-bb3f2.appspot.com", 
+}
