07a39d96bfc5dc0feb8824c24919c1b297fbd152
u4bi <u4bi@u4bi.com>
2017-02-19 23:28:40 +0900

    phaser ts template render

--- phaser-angular2/src/app/app.component.ts
+++ phaser-angular2/src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+/// <reference path='../../node_modules/phaser/typescript/phaser.d.ts'/>
+
+import {Component} from '@angular/core';
+
+import * as Phaser from 'phaser';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+
+  game: Phaser.Game;
+
+    constructor(){
+      this.game = new Phaser.Game(1280, 850, Phaser.AUTO, 'ingame', {
+          preload: this.preload,
+          create: this.create,
+          update: this.update
+      }); 
+    }
+
+  preload() {
+    this.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    this.game.scale.pageAlignHorizontally = true;
+    this.game.scale.pageAlignVertically = true;
+  }
+
+  create() {
+
+  }
+
+  update() {
+
+  }
+
 }
