dde240491e9ef5b873d89012e0a52e347df1dcb2
u4bi <u4bi@u4bi.com>
2017-02-20 22:27:45 +0900

    book form model data binding

--- form-validation-web/src/app/book-form/book-form.component.ts
+++ form-validation-web/src/app/book-form/book-form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Book } from '../book.model';
+
 @Component({
   selector: 'book-form',
   templateUrl: './book-form.component.html',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookFormComponent implements OnInit {
 
+  model = new Book(1, '', '', 'http://');
+
   constructor() { }
 
   ngOnInit() {
+
+  }
+
+  /* http://www.codingdefined.com/2015/05/get-and-set-property-in-typescript.html */
+  
+  get currentBook(){
+    return JSON.stringify(this.model);
   }
 
 }
