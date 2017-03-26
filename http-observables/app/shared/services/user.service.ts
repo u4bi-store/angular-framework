dc0857acaa34a71f753c7659981189fa9fbde6ba
u4bi <u4bi@u4bi.com>
2017-03-26 13:44:10 +0900

    user servie observable render

--- http-observables/app/shared/services/user.service.ts
+++ http-observables/app/shared/services/user.service.ts
@@ -1,17 +1,21 @@
 import { Injectable } from '@angular/core';
 
 import { Http } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+
+import { User } from '../modules/user';
 
 @Injectable()
 export class UserService {
+    private userUrl : string = 'https://reqres.in/api/users';
 
-    constructor(private http : Http){
+    constructor(private http : Http){}
 
-    }
-
-    // all user
-    getUsers(){
-        return this.http.get('https://reqres.in/api/users')
+    /**
+     * 모든 유저의 데이터를 가져온다.
+     */
+    getUsers() : Observable<User[]>{
+        return this.http.get(this.userUrl)
             .map(res => res.json().data);
     }
     // get user
