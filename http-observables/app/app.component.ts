230a0e1667a852a6f12739e7d7e743a274dfbe69
u4bi <u4bi@u4bi.com>
2017-03-26 12:31:53 +0900

    api data binding

--- http-observables/app/app.component.ts
+++ http-observables/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Http } from '@angular/http';
-
+import { User } from './shared/modules/user';
 
 @Component({
   selector: 'my-app',
@@ -9,9 +9,16 @@ import { Http } from '@angular/http';
       <h1>The App Lives!</h1>
       <p>{{ message }}</p>
     </div>
+
+    <div *ngIf="users">
+      <div *ngFor="let user of users">
+        <h2> {{ user.first_name }} {{ user.last_name }}</h2>
+      </div>
+    </div>
   `
 })
 export class AppComponent implements OnInit {
+  users : User[];
 
   constructor(private http: Http){
     
@@ -22,6 +29,8 @@ export class AppComponent implements OnInit {
       .subscribe(data => {
         console.log('API 출력');
         console.log(data);
+
+        this.users = data.json().data;
         
       });
   }
