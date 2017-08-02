7a6a7eefe693f1ecf3616d3414347d38d9ad771b
u4bi <u4bi@u4bi.com>
2017-08-02 23:46:54 +0900

    app module render

--- firebase-offline/src/app/app.module.ts
+++ firebase-offline/src/app/app.module.ts
@@ -3,11 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireOfflineModule } from 'angularfire2-offline';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyAIZtk4_5tdjQmLI12ob59AX7qDkAwj4xs",
+  authDomain: "offline-2ee6d.firebaseapp.com",
+  databaseURL: "https://offline-2ee6d.firebaseio.com",
+  storageBucket: "offline-test"
+};
+
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
+  imports: [ 
+    AngularFireDatabaseModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireOfflineModule,    
     BrowserModule
   ],
   providers: [],
